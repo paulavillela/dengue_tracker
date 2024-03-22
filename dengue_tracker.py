161e46741be26796ce57cd6e233917996adc3cdb
paula villela <1paulavi@gmail.com>
2024-03-22 14:50:07 -0300

    Updated data retrieval method and added number formatting for better visualization

--- dengue_tracker.py
+++ dengue_tracker.py
@@ -2,7 +2,10 @@ import pandas as pd
 import streamlit as st
 from datetime import datetime, date, timedelta
 import geopandas as gpd
+import xlrd
 import plotly.express as px
+import zipfile
+import requests
 
 def write_about():
     # Writes information about the data source
@@ -12,9 +15,21 @@ def write_about():
                     [IBGE](<https://www.ibge.gov.br/>).
             ''')
 
+def format_number(num):
+    # Formats large numbers representing dengue cases in Minas Gerais for better readability
+    if num > 1000 and num < 1000000:
+        if not num % 1000:
+            return f'{num // 1000} K'
+        return f'{round(num / 1000, 2)} M'
+    if num > 1000000:
+        if not num % 1000000:
+            return f'{num // 1000000} M'
+        return f'{round(num / 1000000, 1)} M'
+
 def write_total_cases(df):
     # Writes the total number of dengue cases in Minas Gerais in the sidebar 
     total_cases = df['casos'].sum()
+    total_cases = format_number(total_cases)
     st.sidebar.write('Total cases: ', str(total_cases))
 
 def plot_map(df, gdf):
@@ -44,10 +59,19 @@ def plot_map(df, gdf):
     )
     st.plotly_chart(fig)
 
+def create_geodata_json_file():
+    # Creates the json file containing the 
+    shapefile_path = './data/geodata_city_locations/MG_Municipios_2022.shp'
+    gdf = gpd.read_file(shapefile_path)
+    gdf.rename(columns={'CD_MUN': 'geocode'}, inplace=True)
+    gdf = gdf[['geocode', 'geometry']]
+    gdf.to_file('./data/geodata_city_locations.geojson', driver='GeoJSON')
+
 @st.cache_data
 def get_geodataframe():
     # Gets the geometries of Minas Gerais cities for the map
-    gdf = gpd.read_file('./data/municipios_mg.geojson')
+    create_geodata_json_file()
+    gdf = gpd.read_file('./data/geodata_city_locations.geojson')
     gdf.set_index('geocode', inplace=True)
     return gdf
 
@@ -132,8 +156,7 @@ def get_data_parameters(geocode, year, epidemiological_week):
     return url, geocode, disease, format, ew_start, ew_end, ey_start, ey_end
 
 def get_city_data(city_geocodes, year, week):
-    # Creates a data list with data from all Minas Gerais cities in the specified year and
-    # epidemiological week
+    # Creates a data list with data from all Minas Gerais cities in the specified year and epidemiological week
     data_list = list()
     for geocode in city_geocodes['geocode']:
         url = get_data_url(*get_data_parameters(geocode, year, week))
@@ -143,10 +166,39 @@ def get_city_data(city_geocodes, year, week):
     return df
 
 def get_city_geocodes():
-    # Gets the city codes for the state of Minas Gerais according to IBGE
-    df = pd.read_csv('./data/mg_city_geocodes.csv')
+    # Creates the dataframe containing the city codes for the state of Minas Gerais according to IBGE
+    df = pd.read_excel('./data/city_geocodes/RELATORIO_DTB_BRASIL_MUNICIPIO.xls', skiprows=6)
+    df = df[df['Nome_UF'] == 'Minas Gerais']
+    df = df[['Nome_Município', 'Código Município Completo']]
+    df.rename(columns={'Nome_Município': 'city_name', 'Código Município Completo':'geocode'}, inplace=True)
     df['geocode'] = df['geocode'].astype(str)
     return df
+
+def unzip_file(file, extracted_folder):
+    # Unzips the file downloaded to be used
+    with zipfile.ZipFile(file, 'r') as zip_ref:
+        zip_ref.extractall(extracted_folder)
+
+def get_zip_file(zip_url, save_path, extracted_folder):
+    # Downloads the zip files used on the application
+    response = requests.get(zip_url)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as f:
+            f.write(response.content)
+    else:
+        st.write('Failed to download the file. Status code:', response.status_code)
+    unzip_file(save_path, extracted_folder)
+
+def download_additional_data():
+    # Downloads additional data required for the application
+    city_geocodes_zip_url = 'https://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/divisao_territorial/2022/DTB_2022.zip'
+    city_geocodes_save_path = './data/city_geocodes.zip'
+    city_geocodes_extracted_folder = './data/city_geocodes'
+    geodata_zip_url = 'https://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2022/UFs/AC/AC_Municipios_2022.zip'
+    geodata_save_path = './data/geodata_city_locations.zip'
+    geodata_extracted_folder = './data/geodata_city_locations'
+    get_zip_file(city_geocodes_zip_url, city_geocodes_save_path, city_geocodes_extracted_folder)
+    get_zip_file(geodata_zip_url, geodata_save_path, geodata_extracted_folder)
 
 def make_date_slider(week_list):
     # Makes slider widget for selecting the epidemiological week
@@ -188,6 +240,7 @@ def main():
     chosen_year = make_year_select_box()
     weeks_list = get_sundays_from_year(chosen_year)
     chosen_week = make_date_slider(weeks_list)
+    download_additional_data()
     city_geocodes = get_city_geocodes()
     city_data = get_city_data(city_geocodes, chosen_year, chosen_week)
     current_map_dict = pd.merge(city_geocodes, city_data, on='geocode')
